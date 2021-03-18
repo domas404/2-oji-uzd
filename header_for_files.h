64f22802d3519010ded85bfa95d18eadf4fdf9ad
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-03-18 22:14:26 +0200

    Update header_for_files.h

--- header_for_files.h
+++ header_for_files.h
@@ -3,12 +3,12 @@
 
 #include <fstream>
 
-void Generate_txt(int n, int nd);
-void readFile(Studentas M[], int nd, int &ap, string t);
-bool wayToSort(Studentas a, Studentas b);
-void Calculate(Studentas M[], int ap, int nd);
-void outputFile (Studentas M[], int n);
-void openFile(int &nd, int &ap, Studentas M[]);
-void createFile(int &nd, int &ap, Studentas M[]);
+void generateFile(int n, int nd, string name);
+void readFile(vector<Studentas> &M, int nd, string t, string vm);
+void execusionTest(int ap, string title, string result, int nd, string vm);
+void distinctStudents(vector<Studentas> &M, int ap);
+void outputTime(int ap, vector<Studentas> &G, int size, string title, string league);
+void outputFile(vector<Studentas> &M, int n, string t);
+void Clean(Studentas &A);
 
 #endif
