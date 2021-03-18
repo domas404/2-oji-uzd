c1da161c6c331c36eef1fc89da390523035d699c
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-03-18 22:15:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,23 @@
 #include "header.h"
 #include "header_for_files.h"
 
-int main(){
-    Studentas *M = new Studentas[nmax];
-    bool repeat = true; // ciklas kartojamas, kol igis reiksme false
-    int j=0;            // studento indeksas
-    float vid=0;        // namu darbu vidurkis
-    string a, b;        // tarpiniai kintamieji, igyjantys viena is dvieju reiksmiu, pateiktu naudotojui pvz. (y/n)
-    int nd, ap=0;       // nd - namu darbu ivertinimu kiekis, ap - studentu skaicius
-    cout << "Norite duomenis nuskaityti is tekstinio [f]ailo ar [k]onsoles: ";
-    cin >> b;
+int main(int argc, char **argv){
+    string vm;
+    int nd;
+    nd = atoi(argv[1]); // 1 - namu darbu kiekis
+    vm = argv[2];       // 2 - naudoti vidurki ar mediana (v/m)
+    execusionTest(1000, "kursiokai1000.txt", "rezultatai1000.txt", nd, vm);
+    cout << "Press enter to continue";
     cin.ignore();
-    if (b == "f"){
-        cout << "Duomenu failas jau [e]gzistuoja, ar [s]ugeneruoti? ";
-        cin >> a;
-        if (a == "e")
-            openFile(nd, ap, M);
-        else if (a == "s")
-            createFile(nd, ap, M);
-        Calculate(M, ap, nd);
-        clock_t pradzia1, pabaiga1;
-        pradzia1 = clock();
-        outputFile(M, ap);
-        pabaiga1 = clock();
-        cout << setw(45) << left << "Duomenu irasymas i tekstini faila uztruko: ";
-        cout << 1.0 * (pabaiga1 - pradzia1) / CLOCKS_PER_SEC << " s" << endl;
-    }
-    else if (b == "k") {
-        do {
-            readName(M[j]);
-            Namu_darbai(M[j], vid);
-            Egzaminas(M[j]);
-            M[j].final = Final(vid, M[j].egz);
-            sort (M, M+j+1, wayToSort);
-            cout << "Ar norite ivesti kito studento duomenis? (y/n) ";
-            cin >> a;
-            if (a == "n")
-                repeat=false;
-            else j++;
-            vid=0;
-            cin.ignore();
-        } while (repeat);
-        outputConsole(M, j+1);
-    }
-    delete[] M;
+    execusionTest(10000, "kursiokai10000.txt", "rezultatai10000.txt", nd, vm);
+    cout << "Press enter to continue";
+    cin.ignore();
+    execusionTest(100000, "kursiokai100000.txt", "rezultatai100000.txt", nd, vm);
+    cout << "Press enter to continue";
+    cin.ignore();
+    execusionTest(1000000, "kursiokai1000000.txt", "rezultatai1000000.txt", nd, vm);
+    cout << "Press enter to continue";
+    cin.ignore();
+    execusionTest(10000000, "kursiokai10000000.txt", "rezultatai10000000.txt", nd, vm);
     return 0;
 }
