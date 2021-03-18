46c990deac78fca42ba43b65d2361cc0e6d00391
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-03-18 23:00:56 +0200

    Update header_for_files.h

--- header_for_files.h
+++ header_for_files.h
@@ -6,9 +6,9 @@
 void generateFile(int n, int nd, string name);
 void readFile(vector<Studentas> &M, int nd, string t, string vm);
 void execusionTest(int ap, string title, string result, int nd, string vm);
-void distinctStudents(vector<Studentas> &M, int ap);
-void outputTime(int ap, vector<Studentas> &G, int size, string title, string league);
-void outputFile(vector<Studentas> &M, int n, string t);
+void distinctStudents(vector<Studentas> &M, int ap, string vm);
+void outputTime(int ap, vector<Studentas> &G, int size, string title, string league, string vm);
+void outputFile(vector<Studentas> &M, int n, string t, string vm);
 void Clean(Studentas &A);
 
 #endif
