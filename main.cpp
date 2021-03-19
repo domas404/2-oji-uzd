8466fa65c1c3f6b27ac985447ea7a387e6b11dfc
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-03-19 10:00:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ int main(int argc, char **argv){
     int nd;
     nd = atoi(argv[1]); // 1 - namu darbu kiekis
     vm = argv[2];       // 2 - naudoti vidurki ar mediana (v/m)
-    execusionTest(1000, "kursiokai1000.txt", "rezultatai1000.txt", nd, vm);
+    execusionTest(1000, ".\\kursiokai\\kursiokai1000.txt", nd, vm);
     cout << "Press enter to continue";
     cin.ignore();
-    execusionTest(10000, "kursiokai10000.txt", "rezultatai10000.txt", nd, vm);
+    execusionTest(10000, ".\\kursiokai\\kursiokai10000.txt", nd, vm);
     cout << "Press enter to continue";
     cin.ignore();
-    execusionTest(100000, "kursiokai100000.txt", "rezultatai100000.txt", nd, vm);
+    execusionTest(100000, ".\\kursiokai\\kursiokai100000.txt", nd, vm);
     cout << "Press enter to continue";
     cin.ignore();
-    execusionTest(1000000, "kursiokai1000000.txt", "rezultatai1000000.txt", nd, vm);
+    execusionTest(1000000, ".\\kursiokai\\kursiokai1000000.txt", nd, vm);
     cout << "Press enter to continue";
     cin.ignore();
-    execusionTest(10000000, "kursiokai10000000.txt", "rezultatai10000000.txt", nd, vm);
+    execusionTest(10000000, ".\\kursiokai\\kursiokai10000000.txt", nd, vm);
     return 0;
 }
