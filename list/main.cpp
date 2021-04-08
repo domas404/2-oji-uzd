87e15563d4fff75c06f1a3b6faf00d47b43e29b8
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-09 00:11:36 +0300

    Create main.cpp

---
+++ list/main.cpp
@@ -0,0 +1,18 @@
+#include "header.h"
+#include "header_for_files.h"
+
+int main(int argc, char **argv){
+    string vm;
+    int nd;
+    nd = atoi(argv[1]); // 1 - namu darbu kiekis
+    vm = argv[2];       // 2 - naudoti vidurki ar mediana (v/m)
+    string arGen;
+    cout << "Ar generuoti naujus sarasus? [y/n] ";
+    cin >> arGen;
+    execusionTest(1000, ".\\kursiokai\\kursiokai1000.txt", nd, vm, arGen);
+    execusionTest(10000, ".\\kursiokai\\kursiokai10000.txt", nd, vm, arGen);
+    execusionTest(100000, ".\\kursiokai\\kursiokai100000.txt", nd, vm, arGen);
+    execusionTest(1000000, ".\\kursiokai\\kursiokai1000000.txt", nd, vm, arGen);
+    execusionTest(10000000, ".\\kursiokai\\kursiokai10000000.txt", nd, vm, arGen);
+    return 0;
+}
