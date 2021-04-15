852846a0aaeb06698e21e7a8ea4f59fb439acdf0
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 23:18:07 +0300

    Update fileHeader.h

--- deque/fileHeader.h
+++ deque/fileHeader.h
@@ -6,7 +6,7 @@
 void generateFile(int n, int nd, string name);
 void readFile(deque<Studentas> &M, int nd, string t, int ap, void (*vid_med)(Studentas&, int));
 void execusionTest(int ap, string title, int nd, string vm, string arGen);
-void distinctStudents(deque<Studentas> &Kietiakai, int ap, string vm, double &time_taken);
+void distinctStudents(deque<Studentas> &Kietiakai, int ap, string vm);
 void outputTime(int ap, deque<Studentas> &G, string title, string league, string vm);
 void outputFile(deque<Studentas> &M, string t, string vm);
 void Clean(Studentas &A);
