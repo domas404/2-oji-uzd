ceb5218c4097e5dab54a66d56118aff0998a1abe
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 23:20:12 +0300

    Update student_division.cpp

--- vector/student_division.cpp
+++ vector/student_division.cpp
@@ -2,14 +2,14 @@
 #include "fileHeader.h"
 
 // studentu skirstymas i dvi grupes (kietiakus ir varguolius)
-void distinctStudents(vector<Studentas> &Kietiakai, vector<Studentas> &Varguoliai, int ap, string vm, double &time_taken){
+void distinctStudents(vector<Studentas> &Kietiakai, vector<Studentas> &Varguoliai, int ap, string vm){
     auto pr = chrono::high_resolution_clock::now();
     vector<Studentas>::iterator it1;
     it1 = find_if(Kietiakai.begin(), Kietiakai.end(), isLessThan);
     Varguoliai.assign(it1, Kietiakai.end());
     Kietiakai.resize(Kietiakai.size()-Varguoliai.size());
     auto pab = chrono::high_resolution_clock::now();
-    time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
     time_taken *= 1e-9;
     cout << setw(8) << right << ap << setw(50) << left << " studentu skirstymas i dvi grupes uztruko: ";
     cout << fixed << time_taken << setprecision(6) << " s" << endl;
