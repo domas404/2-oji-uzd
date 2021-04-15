8f23343c2a33ee43bd4926e165b7c20217753a85
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 23:12:22 +0300

    Add files via upload

---
+++ list/file_output.cpp
@@ -0,0 +1,32 @@
+#include "header.h"
+#include "fileHeader.h"
+
+// skaiciuojamas protingu ir nelabai studentu isvedimo laikas
+void outputTime(int ap, list<Studentas> &G, string title, string league, string vm){
+    league += " rasymas i tekstini faila uztruko: ";
+    auto pr = chrono::high_resolution_clock::now();
+    outputFile(G, title, vm);
+    auto pab = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
+    time_taken *= 1e-9;
+    cout << setw(8) << right << ap << setw(50) << left << league;
+    cout << time_taken << " s" << endl;
+}
+// i tekstini faila isvedami rezultatai
+void outputFile(list<Studentas> &M, string t, string vm){
+    ofstream r(t);
+    int k=57, p=18;
+    r << setw(p) << left << "Vardas" << setw(p) << left << "Pavarde";
+    if (vm == "v")
+        r << setw(20) << left << "Galutinis (Vid.)" << endl;
+    else if (vm == "m")
+        r << setw(20) << left << "Galutinis (Med.)" << endl;
+    for (int i=0; i<k; i++)
+        r << "-";
+    r << endl;
+    for (list<Studentas>::iterator it = M.begin(); it!=M.end(); ++it){
+        r << setw(p) << left << (*it).vardas << setw(p) << left << (*it).pavarde;
+        r << fixed << setprecision(2) << (*it).final << endl;
+    }
+    r.close();
+}
