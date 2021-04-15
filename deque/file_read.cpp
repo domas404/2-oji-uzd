e4266da36884fced8ba40554e0fe5916f63bb99f
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 23:00:23 +0300

    Add files via upload

---
+++ deque/file_read.cpp
@@ -0,0 +1,46 @@
+#include "header.h"
+#include "fileHeader.h"
+
+// isvalomi tarpinio strukturos kintamojo duomenys
+void Clean(Studentas &A){
+    A.vardas="";
+    A.pavarde="";
+    A.ND.clear();
+    A.egz=0;
+}
+// duomenu nuskaitymas is failo
+void readFile(deque<Studentas> &M, int nd, string t, int ap, void (*vid_med)(Studentas&, int)){
+    auto pr = chrono::high_resolution_clock::now();
+    ifstream input(t);
+    int p;
+    string line;
+    Studentas A;
+    try{
+        if (input.is_open()){
+            getline(input, line);
+            while(!input.eof()){
+                getline(input, line);
+                istringstream iss(line);
+                iss >> A.vardas >> A.pavarde;
+                for (int i=0; i<nd; i++){
+                    iss >> p;
+                    A.ND.push_back(p);
+                }
+                iss >> A.egz;
+                (*vid_med)(A, nd);
+                M.push_back(A);
+                Clean(A);
+            }
+        }
+        else throw(t);
+    }
+    catch(string t){
+        cout << "Neimanoma atidaryti failo pavadinimu \"" << t << "\"" << endl;
+    }
+    input.close();
+    auto pab = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
+    time_taken *= 1e-9;
+    cout << setw(8) << right << ap << setw(50) << left << " studentu nuskaitymas ir skaiciavimai uztruko: ";
+    cout << time_taken << " s" << endl;
+}
---
+++ deque/test.cpp
@@ -0,0 +1,27 @@
+#include "header.h"
+#include "fileHeader.h"
+
+// testuojami veiksmai su tam tikro dydzio studentu sarasu
+void execusionTest(int ap, string title, int nd, string vm, string arGen){
+    auto pr = chrono::high_resolution_clock::now();
+    deque<Studentas> M;
+    double time_1, time_2, time_3;
+    double time=0;
+    if (arGen == "y")
+        generateFile(ap, nd, title);
+    if(vm == "v")
+        readFile(M, nd, title, ap, &AssignVid);
+    else
+        readFile(M, nd, title, ap, &AssignMed);
+    sortStudents(M, ap, time_2);
+    distinctStudents(M, ap, vm, time_3);
+    M.clear();
+    //time += time_1 + time_2 + time_3;
+    auto pab = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
+    time_taken *= 1e-9;
+    cout << endl;
+    cout << setw(8) << right << ap << setw(50) << left << " irasu testo laikas: ";
+    cout << time_taken << " s" << endl;
+    cout << endl;
+}
