54676760ec91e101c081acc4ee7ba1a447f9c2c7
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 22:44:54 +0300

    Update fileHeader.h

--- vector/fileHeader.h
+++ vector/fileHeader.h
@@ -5,7 +5,6 @@
 
 void generateFile(int n, int nd, string name);
 void readFile(vector<Studentas> &M, int nd, string t, int ap, void (*vid_med)(Studentas&, int));
-void assignStruct(deque<string> &split, int &nd, vector<Studentas> &M, int ap, void (*vid_med)(Studentas&, int));
 void execusionTest(int ap, string title, int nd, string vm, string arGen);
 void distinctStudents(vector<Studentas> &Kietiakai, vector<Studentas> &Varguoliai, int ap, string vm, double &time_taken);
 void outputTime(int ap, vector<Studentas> &G, string title, string league, string vm);
