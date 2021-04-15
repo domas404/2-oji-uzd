ad52f529b019a39a8e0bc44401d5a03e33355672
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 22:25:38 +0300

    Update header_for_files.h

--- vector/header_for_files.h
+++ vector/header_for_files.h
@@ -4,11 +4,12 @@
 #include <fstream>
 
 void generateFile(int n, int nd, string name);
-void readFile(deque<string> &split, string t, int ap, double &time_taken);
+void readFile(vector<Studentas> &M, int nd, string t, int ap, void (*vid_med)(Studentas&, int));
+void assignStruct(deque<string> &split, int &nd, vector<Studentas> &M, int ap, void (*vid_med)(Studentas&, int));
 void execusionTest(int ap, string title, int nd, string vm, string arGen);
-void distinctStudents(vector<Studentas> &M, int ap, string vm, double &time_taken);
-void outputTime(int ap, vector<Studentas> &G, int size, string title, string league, string vm);
-void outputFile(vector<Studentas> &M, int n, string t, string vm);
+void distinctStudents(vector<Studentas> &Kietiakai, vector<Studentas> &Varguoliai, int ap, string vm, double &time_taken);
+void outputTime(int ap, vector<Studentas> &G, string title, string league, string vm);
+void outputFile(vector<Studentas> &M, string t, string vm);
 void Clean(Studentas &A);
 
 #endif
