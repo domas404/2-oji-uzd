913eae06fc0aa35d813c9289785aca0eee52ac51
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 22:42:02 +0300

    Update student_division.cpp

--- vector/student_division.cpp
+++ vector/student_division.cpp
@@ -4,36 +4,10 @@
 // studentu skirstymas i dvi grupes (kietiakus ir varguolius)
 void distinctStudents(vector<Studentas> &Kietiakai, vector<Studentas> &Varguoliai, int ap, string vm, double &time_taken){
     auto pr = chrono::high_resolution_clock::now();
-    //vector<Studentas> Varguoliai;
     vector<Studentas>::iterator it1;
-    //cout << "ok1" << endl;
     it1 = find_if(Kietiakai.begin(), Kietiakai.end(), isLessThan);
-    //cout << "ok2" << endl;
-    //Varguoliai.reserve(ap*0.5);
-    //cout << "ok3" << endl;
     Varguoliai.assign(it1, Kietiakai.end());
-    //cout << "ok4" << endl;
     Kietiakai.resize(Kietiakai.size()-Varguoliai.size());
-    //cout << "ok5" << endl;
-
-    // vector<Studentas>::iterator it2 = Kietiakai.end()-1;
-    // for(it2; it2!=it1; --it2){
-    //     Varguoliai.push_back(*it2);
-    //     Kietiakai.pop_back();
-        //++size_1;
-        //if (size_1%100000==0) cout << size_1 << endl;
-    //}
-    //M.erase(M.begin(), M.begin()+size_1);
-    //Varguoliai.swap(M);
-
-    
-    //Kietiakai.swap(M);
-    //M.clear();
-    
-    //Varguoliai.assign(it, Kietiakai.end());
-    //Varguoliai.size();
-
-    // 
     auto pab = chrono::high_resolution_clock::now();
     time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
     time_taken *= 1e-9;
@@ -48,5 +22,4 @@ void distinctStudents(vector<Studentas> &Kietiakai, vector<Studentas> &Varguolia
     Kietiakai.clear();
     outputTime(ap, Varguoliai, t+".txt", " varguoliu", vm);
     Varguoliai.clear();
-    //outputTime(ap, Kietiakai, p+".txt", " studentu", vm);
 }
