4c10ba53be9e074b304ab353b77bb2be7d46cf0d
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 23:20:41 +0300

    Update fileHeader.h

--- vector/fileHeader.h
+++ vector/fileHeader.h
@@ -6,7 +6,7 @@
 void generateFile(int n, int nd, string name);
 void readFile(vector<Studentas> &M, int nd, string t, int ap, void (*vid_med)(Studentas&, int));
 void execusionTest(int ap, string title, int nd, string vm, string arGen);
-void distinctStudents(vector<Studentas> &Kietiakai, vector<Studentas> &Varguoliai, int ap, string vm, double &time_taken);
+void distinctStudents(vector<Studentas> &Kietiakai, vector<Studentas> &Varguoliai, int ap, string vm);
 void outputTime(int ap, vector<Studentas> &G, string title, string league, string vm);
 void outputFile(vector<Studentas> &M, string t, string vm);
 void Clean(Studentas &A);
