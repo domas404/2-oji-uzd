77978c90818bb75dc3fe15592136a55192fd7145
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 23:15:33 +0300

    Update test.cpp

--- vector/test.cpp
+++ vector/test.cpp
@@ -4,13 +4,10 @@
 // testuojami veiksmai su tam tikro dydzio studentu sarasu
 void execusionTest(int ap, string title, int nd, string vm, string arGen){
     auto pr = chrono::high_resolution_clock::now();
+    
     vector<Studentas> M;
     vector<Studentas> Varguoliai;
     M.reserve(ap);
-    //Varguoliai.reserve(ap/2);
-    //deque<string> Lines;
-    double time_1, time_2, time_3;
-    double time=0;
     if (arGen == "y")
         generateFile(ap, nd, title);
     if(vm == "v")
@@ -19,8 +16,7 @@ void execusionTest(int ap, string title, int nd, string vm, string arGen){
         readFile(M, nd, title, ap, &AssignMed);
     sortStudents(M, ap, time_2);
     distinctStudents(M, Varguoliai, ap, vm, time_3);
-    M.clear();
-    //time += time_1 + time_2 + time_3;
+    
     auto pab = chrono::high_resolution_clock::now();
     double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
     time_taken *= 1e-9;
