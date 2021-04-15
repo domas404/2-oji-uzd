caf62c5a0689474b6c3035f292d7372b288d6502
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 23:20:56 +0300

    Update calculations.cpp

--- vector/calculations.cpp
+++ vector/calculations.cpp
@@ -56,11 +56,11 @@ void AssignMed(Studentas &A, int nd){
     A.final = Final(med, A.egz);
 }
 // rusiuojami studentai
-void sortStudents(vector<Studentas> &M, int ap, double &time_taken){
+void sortStudents(vector<Studentas> &M, int ap){
     auto pr = chrono::high_resolution_clock::now();
     sort(M.begin(), M.end(), wayToSort);
     auto pab = chrono::high_resolution_clock::now();
-    time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
     time_taken *= 1e-9;
     cout << setw(8) << right << ap << setw(50) << left << " studentu rikiavimas pagal gal. ivert. uztruko: ";
     cout << time_taken << " s" << endl;
