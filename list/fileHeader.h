1ac539058b4119c44f9a17af07f60d6cc532d4f5
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-15 23:19:26 +0300

    Update fileHeader.h

--- list/fileHeader.h
+++ list/fileHeader.h
@@ -6,7 +6,7 @@
 void generateFile(int n, int nd, string name);
 void readFile(list<Studentas> &M, int nd, string t, int ap, void (*vid_med)(Studentas&, int));
 void execusionTest(int ap, string title, int nd, string vm, string arGen);
-void distinctStudents(list<Studentas> &Kietiakai, int ap, string vm, double &time_taken);
+void distinctStudents(list<Studentas> &Kietiakai, int ap, string vm);
 void outputTime(int ap, list<Studentas> &G, string title, string league, string vm);
 void outputFile(list<Studentas> &M, string t, string vm);
 void Clean(Studentas &A);
