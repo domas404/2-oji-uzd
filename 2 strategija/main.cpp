9480cfa5485100a774793eb9ea723c4c66ca6d57
Dominykas G <64153745+domas404@users.noreply.github.com>
2021-04-17 21:40:51 +0300

    Add files via upload

--- 2 strategija/main.cpp
+++ 2 strategija/main.cpp
@@ -1,1 +1,40 @@
+#include "header.h"
+#include "fileHeader.h"
 
+int main(int argc, char **argv){
+    string vm;
+    int nd;
+    nd = atoi(argv[1]); // 1 - namu darbu kiekis
+    vm = argv[2];       // 2 - naudoti vidurki ar mediana (v/m)
+    string arGen;
+    cout << "Ar generuoti naujus sarasus? [y/n] ";
+    cin >> arGen;
+    cout << "I kuri konteineri talpinti duomenis? "<< endl;
+    cout << "[1] vector\n[2] deque\n[3] list\n";
+    int type;
+    cin >> type;
+    switch(type){
+        case 1:
+            testVector(1000, ".\\kursiokai\\kursiokai1000.txt", nd, vm, arGen);
+            testVector(10000, ".\\kursiokai\\kursiokai10000.txt", nd, vm, arGen);
+            testVector(100000, ".\\kursiokai\\kursiokai100000.txt", nd, vm, arGen);
+            testVector(1000000, ".\\kursiokai\\kursiokai1000000.txt", nd, vm, arGen);
+            testVector(10000000, ".\\kursiokai\\kursiokai10000000.txt", nd, vm, arGen);
+            break;
+        case 2:
+            testDeque(1000, ".\\kursiokai\\kursiokai1000.txt", nd, vm, arGen);
+            testDeque(10000, ".\\kursiokai\\kursiokai10000.txt", nd, vm, arGen);
+            testDeque(100000, ".\\kursiokai\\kursiokai100000.txt", nd, vm, arGen);
+            testDeque(1000000, ".\\kursiokai\\kursiokai1000000.txt", nd, vm, arGen);
+            testDeque(10000000, ".\\kursiokai\\kursiokai10000000.txt", nd, vm, arGen);
+            break;
+        case 3:
+            testList(1000, ".\\kursiokai\\kursiokai1000.txt", nd, vm, arGen);
+            testList(10000, ".\\kursiokai\\kursiokai10000.txt", nd, vm, arGen);
+            testList(100000, ".\\kursiokai\\kursiokai100000.txt", nd, vm, arGen);
+            testList(1000000, ".\\kursiokai\\kursiokai1000000.txt", nd, vm, arGen);
+            testList(10000000, ".\\kursiokai\\kursiokai10000000.txt", nd, vm, arGen);
+            break;
+    }
+    return 0;
+}
---
+++ 2 strategija/sort.cpp
@@ -0,0 +1,33 @@
+#include "header.h"
+#include "fileHeader.h"
+
+// rusiuojami vector elementai
+void sortStudents(vector<Studentas> &M, int ap){
+    auto pr = chrono::high_resolution_clock::now();
+    sort(M.begin(), M.end(), wayToSort);
+    auto pab = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
+    time_taken *= 1e-9;
+    cout << setw(8) << right << ap << setw(50) << left << " studentu rikiavimas pagal gal. ivert. uztruko: ";
+    cout << time_taken << " s" << endl;
+}
+// rusiuojami vector elementai
+void sortStudents(deque<Studentas> &M, int ap){
+    auto pr = chrono::high_resolution_clock::now();
+    sort(M.begin(), M.end(), wayToSort);
+    auto pab = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
+    time_taken *= 1e-9;
+    cout << setw(8) << right << ap << setw(50) << left << " studentu rikiavimas pagal gal. ivert. uztruko: ";
+    cout << time_taken << " s" << endl;
+}
+// rusiuojami vector elementai
+void sortStudents(list<Studentas> &M, int ap){
+    auto pr = chrono::high_resolution_clock::now();
+    M.sort(wayToSort);
+    auto pab = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(pab - pr).count();
+    time_taken *= 1e-9;
+    cout << setw(8) << right << ap << setw(50) << left << " studentu rikiavimas pagal gal. ivert. uztruko: ";
+    cout << time_taken << " s" << endl;
+}
